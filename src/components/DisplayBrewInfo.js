2b1621f8ccd4753831d6b97ed2468996dec39f07
Tony_Huang <luck938938@hotmail.com>
2019-07-21 21:47:26 -0400

    add google map

--- src/components/DisplayBrewInfo.js
+++ src/components/DisplayBrewInfo.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const DisplayBrewInfo = ({brew}) => {
-    console.log("inside here!")
+const DisplayBrewInfo = ({brew, center, zoom}) => {
+    const AnyReactComponent = ({ text }) => (
+        <div style={{
+          color: 'white', 
+          background: 'grey',
+          padding: '15px 10px',
+          display: 'inline-flex',
+          textAlign: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '100%',
+          transform: 'translate(-50%, -50%)'
+        }}>
+          {text}
+        </div>
+      );
+    
     return (
         <div>
             <section>
@@ -11,21 +26,19 @@ const DisplayBrewInfo = ({brew}) => {
                 <p>Adress: {brew.street} </p>
                 <p>{brew.city}, {brew.state} {brew.postal_code}</p>
             </section>
-            {/* <section> */}
-                {/* <GoogleMapReact */}
-                    {/* bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }} */}
-                    {/* defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
-                    yesIWantToUseGoogleMapApiInternals
-                    onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+            <section style={{border:"1px solid black", width: '100%', height: '400px'}}>
+                <GoogleMapReact
+                    // bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+                    defaultCenter={center}
+                    defaultZoom={zoom}
                 >
-                <AnyReactComponent
-                    lat={59.955413}
-                    lng={30.337844}
-                    text="Location"
+                <AnyReactComponent 
+                    lat={59.955413} 
+                    lng={30.337844} 
+                    text={brew.name} 
                 />
                 </GoogleMapReact>
-            </section> */}
+            </section> 
         </div>      
     )
 }
